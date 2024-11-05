3812e2bb18bd6d0966fad9c64dab4df3acc77d37
Tri <105915392+TriFoxie@users.noreply.github.com>
2024-11-05 21:20:40 +0000

    Create Users
     - Adding users possible now
     - Can be done through website
     - Rules such as min char length, no matching existing accounts etc...
     - Update create user webpage

--- sab/CreateAccount.php
+++ sab/CreateAccount.php
@@ -13,16 +13,33 @@ if($conn->connect_error){
 }
 
 //User details
-$loginUsername = $_POST["loginUsername"];
-$loginPassword = $_POST["loginPassword"];
+$createUsername = $_POST["createUsername"];
+$createPassword = $_POST["createPassword"];
+$repeatPassword = $_POST["repeatPassword"];
 
-//SQL
-$sql = "INSERT INTO `users` (`name`, `password`, `date`, `xp`, `clanid`) VALUES ('".$loginUsername."', '".$loginPassword."', '".date("dmy")."', '0', '0');";
-$result = $conn->query($sql);
+//Validation
+if($createPassword != $repeatPassword){
+    die("Passwords must match.");
+}
+if(strlen($createUsername) < 3 || strlen($createPassword) < 3){
+    die("Username and password must be at least 3 characters.");
+}
 
-if($result == 1){
-    echo "Account created.";
+//Check if user with username exists
+$sqlCheckName = "SELECT name FROM users WHERE name = '" . $createUsername . "';";
+$checkNameResult = $conn->query($sqlCheckName);
+if($checkNameResult->num_rows > 0){
+    die("User with that name already exists.");
+}
+
+//SQL for creating user
+$sqlCreateUser = "INSERT INTO `users` (`name`, `password`, `date`, `xp`, `clanid`) VALUES ('".$createUsername."', '".$createPassword."', '".date("dmy")."', '0', '0');";
+$creationResult = $conn->query($sqlCreateUser);
+
+//Outputs
+if($creationResult == 1){
+    die("Account created.");
 }else{
-    echo "Failed.";
+    die("Failed.");
 }
 ?>
