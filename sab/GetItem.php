5c8c93ae0220281868f78de8fe75ffd881407815
Tri <105915392+TriFoxie@users.noreply.github.com>
2024-11-12 12:03:18 +0000

    add sab/GetItem.php
    Retrieves 1 item from the 'items' table with all of its data.

---
+++ sab/GetItem.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sab";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Check connection
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//Get item data from database.
+$itemID = $_POST["itemID"];
+$sqlGetItem = "SELECT * FROM items WHERE id = '" . $itemID . "';";
+$item = $conn->query($sqlGetItem);
+
+//Turn item data to string to be used.
+$itemData = "";
+if($item->num_rows > 0){
+    $item = $item->fetch_assoc();
+    $itemData = $itemData . $item["name"] . "," . $item["type"]; //Add more fields here: $item["{database column}"];
+}else{
+    die("Item not found.")//Item with that ID doesn't exist.
+}
+?>
